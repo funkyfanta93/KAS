2379780dc1498bf18027459837a771c5a15654e3
Denni Knudsen <Deknudsen@hotmail.dk>
2019-11-12 09:24:39 +0100

    Deltager get og constructor

--- src/model/Deltager.java
+++ src/model/Deltager.java
@@ -1,5 +1,33 @@
 package model;
 
 public class Deltager {
+	private String navn;
+	private String adresse;
+	private String by;
+	private int telefonnr;
+
+	public Deltager(String navn, String adresse, String by, int telefonnr) {
+		this.navn = navn;
+		this.adresse = adresse;
+		this.by = by;
+		this.telefonnr = telefonnr;
+
+	}
+
+	public String getNavn() {
+		return navn;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public String getBy() {
+		return by;
+	}
+
+	public int getTelefonnr() {
+		return telefonnr;
+	}
 
 }
