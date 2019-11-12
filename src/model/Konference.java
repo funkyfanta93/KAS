b1fe317b397378ed5ea0cc4b88ad98a28e9e4705
Emil <nbrond@gmail.com>
2019-11-12 09:21:39 +0100

    test

--- src/model/Konference.java
+++ src/model/Konference.java
@@ -1,5 +1,38 @@
 package model;
 
 public class Konference {
+	private String navn = "";
+	private String dato = "";
+	private double pris = 0;
+
+	public Konference(String navn, String dato, double pris) {
+		this.navn = navn;
+		this.dato = dato;
+		this.pris = pris;
+	}
+
+	public void setNavn(String navn) {
+		this.navn = navn;
+	}
+
+	public String getNavn() {
+		return navn;
+	}
+
+	public void setDato(String dato) {
+		this.dato = dato;
+	}
+
+	public String getDato() {
+		return dato;
+	}
+
+	public void setPris(double pris) {
+		this.pris = pris;
+	}
+
+	public double getPris() {
+		return pris;
+	}
 
 }
--- src/model/Udflugt.java
+++ src/model/Udflugt.java
@@ -1,5 +1,37 @@
 package model;
 
 public class Udflugt {
+	private double pris = 0;
+	private String frokost = "";
+	private String dato = "";
 
+	public Udflugt(double pris, String frokost, String dato) {
+		this.pris = pris;
+		this.frokost = frokost;
+		this.dato = dato;
+	}
+
+	public void setPris(double pris) {
+		this.pris = pris;
+	}
+
+	public double getPris() {
+		return pris;
+	}
+
+	public void setFrokost(String frokost) {
+		this.frokost = frokost;
+	}
+
+	public String getFrokost() {
+		return frokost;
+	}
+
+	public void setDato(String dato) {
+		this.dato = dato;
+	}
+
+	public String getDato() {
+		return dato;
+	}
 }
