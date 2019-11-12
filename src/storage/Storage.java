6f3c3cd153ac2ed28eb58c0aa6bfeddf944f6beb
Denni Knudsen <Deknudsen@hotmail.dk>
2019-11-12 11:54:42 +0100

    Storage Arrays done

--- src/storage/Storage.java
+++ src/storage/Storage.java
@@ -2,7 +2,13 @@ package storage;
 
 import java.util.ArrayList;
 
+import model.Deltager;
 import model.Firma;
+import model.Hotel;
+import model.Konference;
+import model.Tilmelding;
+import model.Tilvalg;
+import model.Udflugt;
 
 public class Storage {
 
@@ -14,6 +20,94 @@ public class Storage {
 
 	public static void addFirma(Firma firma) {
 		firmaer.add(firma);
+	}
+
+	public static void removeFirma(Firma firma) {
+		firmaer.remove(firma);
+	}
+
+	private static ArrayList<Deltager> deltagerer = new ArrayList<>();
+
+	public static ArrayList<Deltager> getDeltager() {
+		return new ArrayList<>(deltagerer);
+	}
+
+	public static void addDeltager(Deltager deltager) {
+		deltagerer.add(deltager);
+	}
+
+	public static void removeDeltager(Deltager deltager) {
+		deltagerer.remove(deltager);
+	}
+
+	private static ArrayList<Udflugt> udflugter = new ArrayList<>();
+
+	public static ArrayList<Udflugt> getUdflugt() {
+		return new ArrayList<>(udflugter);
+	}
+
+	public static void addUdflugt(Udflugt udflugt) {
+		udflugter.add(udflugt);
+	}
+
+	public static void removeUdflugt(Udflugt udflugt) {
+		udflugter.remove(udflugt);
+	}
+
+	private static ArrayList<Tilvalg> muligeTilvalg = new ArrayList<>();
+
+	public static ArrayList<Tilvalg> getTilvalg() {
+		return new ArrayList<>(muligeTilvalg);
+	}
+
+	public static void addTilvalg(Tilvalg tilvalg) {
+		muligeTilvalg.add(tilvalg);
+	}
+
+	public static void removeTilvalg(Tilvalg tilvalg) {
+		muligeTilvalg.remove(tilvalg);
+	}
+
+	private static ArrayList<Hotel> hoteller = new ArrayList<>();
+
+	public static ArrayList<Hotel> getHotel() {
+		return new ArrayList<>(hoteller);
+	}
+
+	public static void addHotel(Hotel hotel) {
+		hoteller.add(hotel);
+	}
+
+	public static void removeHotel(Hotel hotel) {
+		hoteller.remove(hotel);
+	}
+
+	private static ArrayList<Konference> konferencer = new ArrayList<>();
+
+	public static ArrayList<Konference> getKonference() {
+		return new ArrayList<>(konferencer);
+	}
+
+	public static void addKonference(Konference konference) {
+		konferencer.add(konference);
+	}
+
+	public static void removeKonfernce(Konference konference) {
+		konferencer.remove(konference);
+	}
+
+	private static ArrayList<Tilmelding> tilmeldinger = new ArrayList<>();
+
+	public static ArrayList<Tilmelding> getTilmelding() {
+		return new ArrayList<>(tilmeldinger);
+	}
+
+	public static void addTilmelding(Tilmelding tilmelding) {
+		tilmeldinger.add(tilmelding);
+	}
+
+	public static void removeTilmelding(Tilmelding tilmelding) {
+		tilmeldinger.remove(tilmelding);
 	}
 
 }
