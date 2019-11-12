5b456d0d50d7c099758a79c941619dfa3256a38e
Denni Knudsen <Deknudsen@hotmail.dk>
2019-11-12 09:37:33 +0100

    get og set hotel

--- src/model/Hotel.java
+++ src/model/Hotel.java
@@ -1,5 +1,28 @@
 package model;
 
 public class Hotel {
+	private String navn;
+	private int pris;
+
+	public Hotel(String navn, int pris) {
+		this.navn = navn;
+		this.pris = pris;
+	}
+
+	public String getNavn() {
+		return navn;
+	}
+
+	public void setNavn(String navn) {
+		this.navn = navn;
+	}
+
+	public int getPris() {
+		return pris;
+	}
+
+	public void setPris(int pris) {
+		this.pris = pris;
+	}
 
 }
