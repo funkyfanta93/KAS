2d3787980345400e7dcf53b4c21bf6994f560cf5
Denni Knudsen <Deknudsen@hotmail.dk>
2019-11-12 09:55:30 +0100

    getter og setter tilmelding

--- src/model/Tilmelding.java
+++ src/model/Tilmelding.java
@@ -1,5 +1,43 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Tilmelding {
+	private String ledsagerNavn;
+	private boolean ledsager;
+	private LocalDate ankomstDato;
+	private LocalDate afrejseDato;
+
+	public String getLedsagerNavn() {
+		return ledsagerNavn;
+	}
+
+	public void setLedsagerNavn(String ledsagerNavn) {
+		this.ledsagerNavn = ledsagerNavn;
+	}
+
+	public boolean isLedsager() {
+		return ledsager;
+	}
+
+	public void setLedsager(boolean ledsager) {
+		this.ledsager = ledsager;
+	}
+
+	public LocalDate getAnkomstDato() {
+		return ankomstDato;
+	}
+
+	public void setAnkomstDato(LocalDate ankomstDato) {
+		this.ankomstDato = ankomstDato;
+	}
+
+	public LocalDate getAfrejseDato() {
+		return afrejseDato;
+	}
+
+	public void setAfrejseDato(LocalDate afrejseDato) {
+		this.afrejseDato = afrejseDato;
+	}
 
 }
